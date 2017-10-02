e74cfba826e509bdb3b9d79dd20d94cf9fda68b2
Bobby Lux <robert.michael.lux@gmail.com>
2017-10-02 16:43:29 -0400

    crazy

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// Import the interface to Tessel hardware
+const tessel = require('tessel');
+const av = require('tessel-av');
+const http = require('http');
+const ambientlib = require('ambient-attx4');
+
+const camera = new av.Camera();
+const takePicture = camera.capture();
+const ambient = ambientlib.use(tessel.port['A']);
+
+ambient.on('ready', function () {
+  // Get points of light and sound data.
+   setInterval( function () {
+     ambient.getLightLevel( function(err, lightdata) {
+      if (err) throw err;
+      else if (lightdata > 0.2) {
+        takePicture.on('data', (image) => {
+          console.log('taking picture');
+
+          const request = http.request({
+            hostname: '172.16.22.196',
+            port: 1337,
+            path: '/pic',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'image/jpg',
+              'Content-Length': image.length
+            }
+          })
+
+          request.write(image)
+
+        })
+      }
+      console.log("Light level:", lightdata.toFixed(8));
+     });
+   }, 500); // The readings will happen every .5 seconds
+ });
+
+ ambient.on('error', function (err) {
+   console.log(err);
+ });
+
+takePicture.on('error', (err) => console.error(err))
---
+++ server-comp.js
@@ -0,0 +1,53 @@
+const app = require('express')();
+const fs = require('fs');
+const http = require('http');
+
+
+
+app.get('/pictest', (req, res, next) => {
+  console.log('ererereerere');
+  var params = {
+    "returnFaceId": "true",
+    "returnFaceLandmarks": "false",
+    "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
+  };
+  const request = http.request({
+    hostname: "https://westcentralus.api.cognitive.microsoft.com",
+    port: 443,
+    path: '/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false&returnFaceAttributes=age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'image/jpg'
+    }
+  })
+  fs.readFile('./test.jpg', 'image/jpg', function(err, data) {
+    request.write(data);
+    console.log('trying to put data..')
+    if(err) console.error(err);
+  });
+
+  res.send("SENT");
+})
+
+app.post('/pic', (req, res, next) => {
+	console.log('Request incoming')
+	var imageThing = new Buffer(0);
+
+
+	req.on('data', (chunk) => {
+		imageThing = Buffer.concat([imageThing, chunk])
+	})
+
+	req.on('end', () => {
+		fs.writeFile('./kate.jpg', imageThing)
+	})
+
+})
+
+app.get('/pic', (req, res, next) => {
+	res.sendFile(__dirname + '/kate.jpg');
+})
+
+app.listen(1337, () => {
+	console.log('rhyming at 1337')
+})
